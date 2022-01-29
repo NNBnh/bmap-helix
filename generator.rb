ffa2f80c64c75c1d015d6c3584f32060f4db8204
NNB <n.at@aleeas.com>
2022-01-29 13:56:36 +0000

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,72 +1,43 @@
+def dwim_insert(main_cmd, ins_cmd = nil)
+  if ins_cmd.nil?
+    {
+      normal: ["collapse_selection", main_cmd],
+      select: ["delete_selection_noyank", main_cmd]
+    }
+  else
+    {
+      normal: ["collapse_selection", main_cmd],
+      select: ["delete_selection_noyank", main_cmd],
+      insert: ins_cmd
+    }
+  end
+end
 
-dwim_exit = {
-  normal: "collapse_selection",
-  select: "normal_mode",
-  insert: "normal_mode"
-}
-
-#TODO
-# "i" = "extend_line_up"
-# "k" = "extend_line_down"
-# "j" = "extend_char_left"
-# "l" = "extend_char_right"
-# "u" = "extend_prev_word_start"
-# "U" = "extend_prev_long_word_start"
-# "o" = "extend_next_word_end"
-# "O" = "extend_next_long_word_end"
-
-# [keys.select."space"]
-# "i" = "goto_file_start"
-# "I" = "scroll_up"
-# "k" = "goto_file_end"
-# "K" = "scroll_down"
-# "j" = "extend_to_line_start"
-# "l" = "extend_to_line_end"
-# "u" = "goto_first_nonwhitespace"
-
-target_next = {
-  normal: "search_next",
-  select: "extend_search_next"
-}
-
-target_prev = {
-  normal: "search_prev",
-  select: "extend_search_prev"
-}
-
-dwim_insert = {
-  normal: "insert_mode"
-  select: ["delete_selection_noyank", "insert_mode"],
-  insert: "completion"
-}
-
-dwim_paste = {
-  normal: "paste_before",
-  select: ["delete_selection_noyank", "paste_before"]
-}
-
-dwim_shell = {
-  normal: "shell_insert_output",
-  select: ["delete_selection_noyank", "shell_insert_output"]
-}
-
-dwim_select = {
-  normal: "select_mode",
-  select: "extend_line"
-}
+def dwim_movement(cmd)
+  {
+    normal: cmd,
+    select: cmd.gsub(/\A(move_|go|)/, "extend_")
+  }
+end
 
 map = {
-  exit: [dwim_exit],
+  exit: [
+    {
+      normal: "collapse_selection",
+      select: "normal_mode",
+      insert: "normal_mode"
+    }
+  ],
   area: ["goto_next_buffer", "goto_previous_buffer"],
 
-  up: ["move_line_up", "copy_selection_on_prev_line"],
-  down: ["move_line_down", "copy_selection_on_next_line"],
-  left: ["move_char_left", "jump_backward"],
-  right: "move_char_right", "jump_forward"],
-  backward: ["move_prev_word_start", "move_prev_long_word_start"],
-  forward: ["move_next_word_end", "move_next_long_word_end"],
+  up: [dwim_movement("move_line_up"), "copy_selection_on_prev_line"],
+  down: [dwim_movement("move_line_down"), "copy_selection_on_next_line"],
+  left: [dwim_movement("move_char_left"), "jump_backward"],
+  right: [dwim_movement("move_char_right"), "jump_forward"],
+  backward: [dwim_movement("move_prev_word_start"), dwim_movement("move_prev_long_word_start")],
+  forward: [dwim_movement("move_next_word_end"), dwim_movement("move_next_long_word_end")],
 
-  primary: [dwim_insert, "code_action"],
+  primary: [dwim_insert("insert_mode", "completion"), "code_action"],
   secondary: ["search", "rsearch"],
   tertiary: ["extend_line", "extend_to_line_bounds"],
   alt_primary: ["replace", "rename_symbol"],
@@ -76,9 +47,9 @@ map = {
   time: ["undo", "redo"],
   cut: ["delete_selection", "delete_selection_noyank"],
   in: ["yank"],
-  out: [dwim_paste, :todo],
+  out: [dwim_insert("paste_before"), :todo],
 
-  target: [target_next, target_prev],
+  target: [dwim_movement("search_next"), dwim_movement("search_prev")],
   bookmark: [:todo, :todo],
   new: ["add_newline_below", "add_newline_above"],
   play: ["replay_macro", "record_macro"],
@@ -107,9 +78,15 @@ map = {
   num_0: [:todo],
 
   assign: ["select_register"],
-  command: ["command_mode", dwim_shell],
+  command: ["command_mode", dwim_insert("shell_insert_output")],
 
-  focus: [dwim_select, "select_all"],
+  focus: [
+    {
+      normal: "select_mode",
+      select: "extend_line"
+    },
+    "select_all"
+  ],
   self: ["flip_selections", "ensure_selections_forward"],
   cycle: ["rotate_selections_forward", "rotate_selections_backward"],
   item: ["rotate_selection_contents_backward", "rotate_selection_contents_forward"],
@@ -122,11 +99,11 @@ map = {
     enter: ["split_selection_on_newline"],
     area: ["buffer_picker", "file_picker"],
 
-    up: ["goto_file_start", "scroll_up"],
-    down: ["goto_file_end", "scroll_down"],
-    left: ["extend_to_line_start", :todo],
-    right: ["extend_to_line_end_newline", :todo],
-    backward: ["goto_first_nonwhitespace", "page_up"],
+    up: ["goto_file_start", "scroll_up"], #TODO
+    down: ["goto_file_end", "scroll_down"], #TODO
+    left: [dwim_movement("goto_line_start"), :todo],
+    right: [dwim_movement("goto_line_end_newline"), :todo],
+    backward: ["goto_first_nonwhitespace", "page_up"], #TODO
     forward: [nil, "page_down"],
 
     primary: ["surround_add"],
